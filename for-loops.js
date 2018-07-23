c6c48982c827d01bd508cb120fc74f99948c3b51
bananaboots <gracekang.93@gmail.com>
2018-07-23 16:56:13 -0400

    Revised assignment for-loops

--- for-loops.js
+++ for-loops.js
@@ -1,50 +1,32 @@
-// example:
-// Write a for loop that logs the numbers 0 through 5
 for (let i = 0; i < 6; i++) {
   console.log(i);
 }
 
 
-// 1.
-// Write a for loop that logs the numbers 0 through 10
 for (let i = 0; i < 11; i++) {
   console.log(i);
 }
 
 
-
-// 2.
-// Write a loop that logs the numbers 10 through 0
 for (let i = 10; i >= 0; i--) {
   console.log(i);
 }
 
 
-// 3.
-// Write a loop that logs the word "Hippopotamus" 17 times.
 for (let i = 0; i < 18; i++) {
   console.log('Hippopotamus');
 }
 
 
-// 4.
-// Write a for loop that logs 2, 4, 6, 8, 10
 for (let i = 2; i < 11; i = i + 2) {
   console.log(i);
 }
 
 
-// 5.
-// Write a loop that logs the numbers 2, 4, 6, 8.
-// Do not edit the existing code.
-
 for (let i = 0; i < 4; i++) {
   console.log(i * 2 + 2);
 }
 
-// 6.
-// Write a loop that logs the numbers 5 through 10.
-// Do not edit the existing code.
 
 for (let i = 0; i < 20; i++) {
   if (i > 4 && i < 11) {
@@ -52,11 +34,6 @@ for (let i = 0; i < 20; i++) {
   }
 }
 
-// 7.
-// Write a loop that logs "Marco!" when i is even,
-// "Polo!" when i is odd,
-// and 'Game Over!' when i is 0.
-// Do not edit the existing code.
 
 for (let i = 10; i >= 0; i--) {
   if ((i > 0) && (i % 2 === 0)) {
@@ -68,10 +45,7 @@ for (let i = 10; i >= 0; i--) {
   }
 }
 
-// 8.
-// Use an else-if statement to assign all four null variables based on the age variable.
-// The code should work regardless of what value age is assigned to.
-// Assume the driving age is 16, voting age is 18, drinking age is 21, and retirement age is 66.
+
 const age = 26;
 let canDrive = null;
 let canVote = null;
@@ -79,7 +53,7 @@ let canDrink = null;
 let canRetire = null;
 
 
-// Write all of your code below. Do not change the code above.
+
 if (age < 16) {
   canDrive = false;
   canVote = false;
@@ -109,10 +83,7 @@ if (age < 16) {
 
 
 
-// STRETCH PROBLEMS (NOT REQUIRED)
 
-// 9.
-// Find a second way to write the loop from question 4.
 for (let i = 2; i < 11; i++) {
   if (i % 2 === 0) {
     console.log(i);
@@ -120,10 +91,6 @@ for (let i = 2; i < 11; i++) {
 }
 
 
-// 10.
-// Write a loop that logs the numbers 0 through 10.
-// Only add code in the parenthesis following the 'for' keyword
-// Finish the code in the parentheses
 
 for (let i = 0; i <= 50; i = i + 5) {
   //Do not add or edit code below here!
@@ -135,12 +102,6 @@ for (let i = 0; i <= 50; i = i + 5) {
     console.log(i/5);
   }
 }
-
-// 11.
-// Imagine you throw two six sided dice and sum up the total.
-// Write a loop that logs every possible score you could roll.
-// Used a nested for loop ( a loop inside of a loop)
-// Bonus Challenge: Don't log repeats.
 
 
 
